34056f1d0e9097eedacc0a032d777f31d8014695
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-03 20:25:20 -0500

    [Add] the initial config for the routes

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const routes = require('./routes/index.routes');
+
 const app = express();
+app.use(express.json());
+
+app.use('/', routes);
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log("listening on port " + PORT);
---
+++ routes/index.routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+// const users = require('./users.routes');
+// const categories = require('./categories.routes');
+// const destinations = require('./destinations.routes');
+// const favorites = require('./favorites.routes');
+// const types = require('./types.routes');
+
+// The principal path
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Travel API' });
+});
+
+// Use the routes
+// router.use('/users', users);
+// router.use('/categories', categories);
+// router.use('/destinations', destinations);
+// router.use('/favorites', favorites);
+// router.use('/types', types);
+
+module.exports = router;
+
