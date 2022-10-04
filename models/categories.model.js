8ade26250d8e11335cdcbf8cc3f9a0e43a7739d8
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-03 19:57:49 -0500

    [Fix] the validate attribute in categories model

--- models/categories.model.js
+++ models/categories.model.js
@@ -5,18 +5,18 @@ const Destination = require('./destinations.model.js')
 const Category = sequelize.define('Category', {
     landscape: {
         type: DataTypes.TEXT,
-        is: /^[a-z]+$/i,
         allowNull: false
     },
     icon: {
         type: DataTypes.TEXT,
         allowNull: false,
-        isUrl: true,
+        validate: {
+            isUrl: true
+        }
         unique: true
     },
     description: {
         type: DataTypes.TEXT,
-        is: /^[a-z]+$/i,
         allowNull: false
     },
 });
