c18827efdc17f7298e60e3130907418e79b06e4d
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-05 10:26:48 -0500

    [Delete] duplicate sequelize declararion in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/db');
 const routes = require('./routes/index.routes');
-const sequelize = require('./config/db');
+
 const app = express();
 app.use(express.json());
 
