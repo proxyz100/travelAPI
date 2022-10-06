e80725a010121ed8a36a71e9675a4c0229ef5e3e
francisco-solis99 <franciscosoliscss@gmail.com>
2022-10-05 23:59:17 -0500

    [Add] the optional middleware

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const sequelize = require('./config/db');
 const routes = require('./routes/index.routes');
+const auth = require('./config/auth.js');
 
 const app = express();
 app.use(express.json());
 
+// Ask for the optional middleware before we use the routes
+app.use(auth.optional); // global middle ware
 app.use('/', routes);
+
 try {
     sequelize.authenticate();
     sequelize.sync();
