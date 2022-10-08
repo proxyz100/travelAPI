0377104f2902e9bd4bafe249063d98a992b0566b
marmelendez <lizbethmelendez@live.com>
2022-10-08 15:04:08 -0500

    Document Types routes

--- routes/types.routes.js
+++ routes/types.routes.js
@@ -1,3 +1,20 @@
+/**
+ * @swagger
+ * definitions:
+ *   Type:
+ *      type: object
+ *      required:
+ *          - name
+ *      properties:
+ *       id:
+ *          type: integer
+ *       name:
+ *          type: string
+ *          required: true
+ *      example:  
+ *          "id": 1
+ *          "name": "Admin"
+ * */
 const router = require('express').Router();
 const auth = require('../config/auth');
 const {
@@ -7,9 +24,114 @@ const {
     deleteType }
     = require('../controllers/types.controller')
 
+/**
+ * @swagger
+ *  tags:
+ *    name: Types
+ *    description: Endpoints for Types
+ */
+
+/**
+ * @swagger
+ *  /types/:
+ *      get:
+ *          summary: Get all Types of users
+ *          tags: [Types]
+ *          security:
+ *              - BearerAuth: []
+ *          responses:
+ *              200:
+ *                  description: A list of all types is displayed.
+ *                  schema:
+ *                      $ref: '#/definitions/Type'
+ *              401:
+ *                  description: Unauthorized - the user is not admin
+ */
 router.get('/', auth.isAdmin, getTypes);
+
+/**
+ * @swagger
+ *  /types/:
+ *      post:
+ *          summary: Create a new Type
+ *          tags: [Types]
+ *          parameters:
+ *              -   in: body
+ *                  name: name
+ *                  description: Name of new Type
+ *                  schema:
+ *                      type: string
+ *                      required: true
+ *                      example: '{"name": "Basic"}'
+ *          responses:
+ *              201:
+ *                  description: New Type has been created
+ *                  schema:
+ *                      $ref: '#/definitions/Type'
+ *              400:
+ *                  description: Bad Request (i.e. missing required field)
+ *              401:
+ *                  description: Unauthorized - the user is not admin             
+ */
 router.post('/', createType);
+
+/**
+ * @swagger
+ *  /types/{id}:
+ *      patch:
+ *          summary: Update Type
+ *          tags: [Types]
+ *          security:
+ *              - BearerAuth: []
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  description: Unique id of Type
+ *                  schema:
+ *                      type: integer
+ *                      required: true
+ *                      example: '1'
+ *              -   in: body
+ *                  name: name
+ *                  description: New name of Type
+ *                  schema:
+ *                      type: string
+ *                      required: true
+ *                      example: '{"name": "Admin"}'
+ *          responses:
+ *              201:
+ *                  description: Type with new name
+ *                  schema:
+ *                      $ref: '#/definitions/Type'
+ *              400:
+ *                  description: Bad Request (i.e. missing required field)
+ *              401:
+ *                  description: Unauthorized - the user is not admin             
+ */
 router.patch('/:id', auth.isAdmin, updateType);
+
+/**
+ * @swagger
+ *  /types/{id}:
+ *      delete: 
+ *          summary: Delete Type by Id
+ *          tags: [Types]
+ *          security:
+ *              - BearerAuth: []
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  description: Unique id of Type
+ *                  schema:
+ *                      type: integer
+ *                      required: true
+ *                      example: '1'
+ *          responses:
+ *              200:
+ *                  description: The type has been deleted.
+ *              401:
+ *                  description: Unauthorized the user is not admin. 
+ */
 router.delete('/:id', auth.isAdmin, deleteType);
 
 module.exports = router;
